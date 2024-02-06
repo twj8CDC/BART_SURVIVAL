127a1f267bf782f7343047b37e68c3419d64e775
twj8CDC <twj8@cdc.gov>
2024-02-06 12:33:30 -0500

    stable package loaded to pip

--- src/bart_survival/__init__.py
+++ src/bart_survival/__init__.py
@@ -1,36 +1,33 @@
-import arviz as az
-import numpy as np
-import scipy.stats as sp
-import sksurv as sks
-import sksurv.metrics as skm
-import sksurv.linear_model
-
-from pymc_experimental.model_builder import ModelBuilder
-from typing import Dict, List, Optional, Tuple, Union, Any
-
-from pathlib import Path
 # import arviz as az
-import pymc as pm
-import pymc_bart as pmb
-import xarray as xr
-# import json
-import scipy.stats as sp
-import warnings
-import pytensor.tensor as pt
-from pymc_bart.utils import _sample_posterior
-import cloudpickle as cpkl
-# import pyspark.sql.functions as F
-# import pyspark.sql.window as W
-import sksurv as sks
-from sksurv import nonparametric
+# import numpy as np
+# import scipy.stats as sp
+# import sksurv as sks
+# import sksurv.metrics as skm
+# import sksurv.linear_model
 
+# from pymc_experimental.model_builder import ModelBuilder
+# from typing import Dict, List, Optional, Tuple, Union, Any
 
-
+# from pathlib import Path
+# # import arviz as az
+# import pymc as pm
+# import pymc_bart as pmb
+# import xarray as xr
+# # import json
+# import scipy.stats as sp
+# import warnings
+# import pytensor.tensor as pt
+# from pymc_bart.utils import _sample_posterior
+# import cloudpickle as cpkl
+# # import pyspark.sql.functions as F
+# # import pyspark.sql.window as W
+# import sksurv as sks
+# from sksurv import nonparametric
 # from causalpy import pymc_experiments, pymc_models, skl_experiments, skl_models
 # from causalpy.version import __version__
-
 # from .data import load_data
 
+from bart_survival.version import __version__
 __all__ = [
     "surv_bart",
     "simulation",
